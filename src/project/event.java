cc7cb87f962b2d53119d6c133f2f7c30259dbc68
yurii2566 <yarawaleed6652@gmail.com>
2026-05-05 17:53:59 +0300

    event

--- src/project/event.java
+++ src/project/event.java
@@ -1,5 +1,38 @@
 package project;
-
+@Entity
 public class event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private String date;
+	public void Event() {}
+	public Event (String name,String description,String date) {
+		this.name = name;
+		this.description = description;
+		this.date = date;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
 
 }
