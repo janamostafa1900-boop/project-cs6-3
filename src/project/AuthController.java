2edf42332f5bbd68b6fb87b8007a247c0815e849
janakhairy88 <janakhairy88@gmail.com>
2026-05-07 13:08:19 +0300

    Update AuthController.java

--- src/project/AuthController.java
+++ src/project/AuthController.java
@@ -1,1 +1,19 @@
+package project;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserRepo userRepo;
 
