5ac2c7e0a63b315dd97115a000cced1f5233bd83
janakhairy88 <janakhairy88@gmail.com>
2026-05-08 22:38:48 +0300

    Update UserRepo.java

--- src/project/UserRepo.java
+++ src/project/UserRepo.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
-    // used to check if username already exists
+   
     User findByUsername(String username);
 
-    // used for login
+    
     User findByUsernameAndPassword(String username, String password);
 }
